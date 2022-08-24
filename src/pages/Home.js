284d3a6afb80f3dd0900f6f2c6d140e8d3afcd1f
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-08-24 23:29:15 +0200

    action button now navigates user to shop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleAction = () => {
+    navigate("/shop");
+  };
+
   return (
     <div className="App">
       <Header />
       <main>
-        <p>home</p>
+        <p>Some Slogan</p>
+        <p>Some Statements</p>
+        <button onClick={handleAction}>Some Shopping</button>
       </main>
       <Footer />
     </div>
